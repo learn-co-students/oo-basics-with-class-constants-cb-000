31e2f7db8651e2d142b9c56410a8ad274c594f41
Maxgrok <maxgrok@Maxgroks-Air.fios-router.home>
2017-01-02 15:16:35 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ class Shoe
   BRANDS = []
 
   def initialize(brand)
-  	@brand = brand # defines instance variable
+  	@brand = brand 
   	if BRANDS.include?(@brand) != true
   		BRANDS << @brand
   	end
