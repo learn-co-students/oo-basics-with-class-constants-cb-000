a047049882bf90ba5ed34b151230af3ad14ede46
marysue <mary@marylark.com>
2019-02-17 12:11:25 -0800

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,9 +2,28 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if unique?(brand)
+      BRANDS << brand
+    end
   end
+  
+  def brand=(brand)
+    @brand=brand
+  end
+
+  def unique?(thisBrand)
+    Shoe::BRANDS.each do |brand|
+      if brand== thisBrand
+         return false;
+      end
+    end
+    return true;
+  end
+
 
   def cobble
     self.condition = "new"
@@ -12,3 +31,10 @@ class Shoe
   end
 
 end
+
+#myShoe = Shoe.new("addidas");
+#anotherShoe = Shoe.new("nikes")
+
+#Shoe::BRANDS.each do |brand|
+# puts("Brand:  #{brand}");
+#end
